36021c72b9f38e7b8031324abc4b2a7773779d9e
kaxadontstarve <103281248+kaxadontstarve@users.noreply.github.com>
2022-04-09 22:58:25 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -12,9 +12,9 @@
 	$password2 = $_POST["password2"];
 
 	$body = $name. ' ' .$login. ' ' .$password1. ' ' .$password2.;
-	$sendMessage->Subject = 'Привет! Это "Фрилансер по жизни"';
+	$sendMessage->Subject = 'Привет! Это "Фрилансер по жизни';
 
-	$sendMessage->addAddress('kaxa0308super@gmail.com');
+	$sendMessage->addAddress('kaxa0308super@kaxadontstarve.github.io');
 	$sendMessage->Body = $body;
 
 	$sendMessage->send();
