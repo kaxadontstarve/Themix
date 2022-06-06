f03c6a891dca8f69108af3c167bf9a96b0a7bf78
kaxadontstarve <103281248+kaxadontstarve@users.noreply.github.com>
2022-06-06 17:18:50 +0300

    Delete src directory

--- src/router/index.js
+++
@@ -1,9 +0,0 @@
-import About from "../pages/About";
-import Posts from '../pages/Posts';
-
-
-export const routes =[
-    { path: '/about', component: About, exact: true },
-    { path: '/posts', component: Posts, exact: true },
-    
-]
